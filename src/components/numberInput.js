490c1612a0893b9f242adb3b389f259cd7ba999c
Dan <danieloleary89@gmail.com>
2018-10-21 13:57:58 +0100

    fix

--- src/components/numberInput.js
+++ src/components/numberInput.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React from "react"
 import PropTypes from "prop-types";
+import Field from './field'
 
 const NumberInput = (props) => (
     <Field label={props.label} control={
